830b0b43ffbd11cfbcc37d4961d35e29ed4bc61e
yongsanghoon <yongsanghoon@gmail.com>
2019-09-28 19:59:33 -0700

    Implemented spawn manager

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -10,7 +10,7 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //RandomSpawn();
+
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _enemyPrefab;
+
+    [SerializeField]
+    private GameObject _enemyContainer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnRoutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Coroutine to wait 5 seconds per enemy spawn
+    private IEnumerator SpawnRoutine()
+    {
+        // Use a infinite while loop
+        while (true)
+        {
+            // Instantiate enemy prefab
+            Vector3 randomPos = new Vector3(Random.Range(-8f, 8f), 7, 0);
+            GameObject newEnemy = Instantiate(_enemyPrefab, randomPos, Quaternion.identity);
+            newEnemy.transform.parent = _enemyContainer.transform;
+
+            // yield for 5 seconds
+            yield return new WaitForSeconds(5);
+        }
+    }
+
+    
+}
